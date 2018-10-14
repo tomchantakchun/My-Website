a605c1a7df4bcd76428e86dd9ca72fbb03071dd8
Tom Chan <ittomchan@gmail.com>
2018-10-15 02:58:06 +0800

    My Website 3

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+var workinprogress = () => {
+    alert("Sorry. Work in Progress.");
+}
+
+$('.box i').hover(function () { $(this).addClass('animated bounce slow delay-1s') });
+
+$('.box i').mouseleave(function () { $(this).removeClass('animated bounce slow delay-1s') })
+
+$('.lastonela i').hover(function () { $(this).addClass('animated bounce slow delay-1s') });
+
+$('.lastonela i').mouseleave(function () { $(this).removeClass('animated bounce slow delay-1s') })
